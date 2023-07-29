37ada810eb6b100c1e095d8d1f35b369109e0f4d
miifayez <fayezmeriam6@gmail.com>
2023-07-29 20:51:54 +0300

    first commit

---
+++ page.js
@@ -0,0 +1,420 @@
+
+//Question 1 
+// var num=window.prompt("enter num");
+// document.write(num)
+
+
+
+//Question 2 
+
+// var num=window.prompt("Enter num ")
+// num=Number(num)                 // b7wl el rkm el user d5lo l number 
+//  if(num%3==0 && num%4==0)
+//  {
+//      document.write("yes divided by 3 & 4 ")
+//  }
+//  else{
+
+//  document.write("No")
+//  }
+
+
+
+//Question 3 
+
+//  var num1=Number(window.prompt("Enter num 1"));
+//  var num2=Number(window.prompt("Enter num2"));
+
+
+//  if(num1>num2){
+//      document.write(num1)
+//  }
+// else
+// {
+// document.write(num2)
+// }
+
+
+
+
+//Question4
+
+// var num=Number(window.prompt("Enter num"));
+// if(num<0)
+// {
+//     document.write("Negative number")
+// }
+// else{
+
+//     document.write("Positive")
+// }
+
+
+
+//Question5
+
+// var num1=Number(window.prompt("Enter num 1"))
+// var num2=Number(window.prompt("Enter num 2"))
+// var num3=Number(window.prompt("Enter num 3"))
+
+
+// if(num1>num2&& num1>num3)
+// {
+//     document.write(num1 +" is the max num")
+// }
+
+// else if (num2>num1&& num2>num3){
+//     document.write(num2 +" is the max num")
+// }
+
+// else if (num3>num1&& num3>num2)
+// {
+//     document.write(num3 + " is the max num")
+// }
+
+
+//  if (num1<num2&&num1<num3)
+// {
+//     document.write(num1 + " is the min num")
+// }
+
+// else if (num2<num1&& num2<num3)
+// {
+//     document.write(num2 + " is the min")
+// }
+
+// else if (num3<num1&&num3<num2)
+// {
+//     document.write(num3 + " is the min")
+// }
+
+
+
+//Question6
+
+// var num=Number(window.prompt("Enter num"))
+// if(num%2==0)
+// {
+// document.write("num is even")
+// }
+// else
+// {
+// document.write("num is odd")
+// }
+
+
+
+//Question 8
+
+
+
+// var char = window.prompt("Enter letter")
+// switch(char){
+// case"a":
+// case"o":
+// case"i":
+// case"e":
+// case"u":
+// document.write("Letter is vouwel ")
+// break;
+// default:
+//     document.write("Letter is cnsenent")
+//     break;
+// }
+
+
+
+
+
+//Question 9
+
+// var num = Number(window.prompt("Enter number"))
+// for(i=1 ; i<=num ; i++){
+//     document.write (i)
+// }
+
+
+//Question 10 
+// var num = Number(window.prompt("Enter number"))
+// for(var i=1 ; i<=12 ; i++){
+//     document.write(i*num)
+// }
+
+//Question 11
+
+// var num = Number(window.prompt("Enter number"))
+// for(var i =1 ; i<=num; i++)
+// if(i%2==0)
+// {
+//     document.write(i)
+// }
+
+
+//Question 12
+
+// var num1 = Number(window.prompt("Enter number"))
+// var num2 = Number(window.prompt("Enter power"))
+
+// document.write(num1**num2)
+
+
+
+//Question 13 
+// var mark1 = Number(window.prompt("Enter mark1"));
+// var mark2 = Number(window.prompt("Enter mark2"));
+// var mark3 = Number(window.prompt("Enter mark3"));
+// var mark4 = Number(window.prompt("Enter mark4"));
+// var mark5 = Number(window.prompt("Enter mark5"));
+
+
+
+
+// total=mark1+mark2+mark3+mark4+mark5;
+// document.write(total)
+
+
+// avg=mark1+mark2+mark3+mark4+mark5/5;
+// document.write(avg)
+
+// percentage=total/500*100
+// document.write(percentage)
+
+//Question 13
+
+// var monthnumber= Number(window.prompt("Enter number"))
+// if (monthnumber==1 || monthnumber==3 || monthnumber==5 || monthnumber==7 || monthnumber==9 || monthnumber==11)
+
+// {
+//     document.write("no of days is 31  ")
+// }
+
+// else(monthnumber==2 || monthnumber==4 || monthnumber==6 || monthnumber== 8 || monthnumber==10 || monthnumber==12)
+ 
+// {
+//     document.write("no of days 30")
+// }
+
+
+
+
+//Question 14
+
+// var num1 = Number(window.prompt("Enter your grade"))
+// var num2 = Number(window.prompt("Enter your grade"))
+// var num3 = Number (window.prompt("Enter your grade"))
+// var num4 = Number(window.prompt("Enter your grade"))
+// var num5 = Number (window.prompt("Enter your grade "))
+
+// percentage=(num1+num2+num3+num4+num5)/500*100;
+
+// if (percentage>=90)
+// {
+//     document.write ("Grade A")
+// }
+// else if (percentage>=80)
+// {
+//     document.write ("Grade B")
+// }
+// else if (percentage>=70)
+// {
+//     document.write ("Grade c")
+// }
+// else if (percentage>=60)
+// {
+//     document.write ("Grade D")
+// }
+// else if (percentage>=40)
+// {
+//     document.write ("Grade E")
+// }
+
+
+// else if (percentage<=40)
+// {
+//     document.write ("Grade F")
+// }
+
+
+
+//Question 15
+
+// var num = Number (window.prompt("Enter month"))
+// switch(num)
+// {
+// case "1":
+// case "3":
+// case "5":
+// case "7": 
+// case "9": 
+// case "11":
+//     document.write("31 days");
+//     default:
+// case "2":
+// case "4":
+// case "6":
+// case "8": 
+// case "10": 
+// case "12":
+//     document.write("30 days");
+//     break;
+// }
+
+
+//Question 16 
+
+
+// var char = window.prompt("Enter letter")
+// switch(char){
+// case"a":
+// case"o":
+// case"i":
+// case"e":
+// case"u":
+// document.write("Letter is vouwel ")
+// break;
+// default:
+//     document.write("Letter is cnsenent")
+//     break;
+// }
+
+
+
+
+
+
+
+//Question 17
+
+//  var num = window.prompt("Enter num")
+//  var num2 = window.prompt("Enter num")
+
+//  switch(num>num2)
+//  {
+
+//    case true :
+//      document.write(num)
+//     break;
+//     case false:
+//         document.write(num2)
+//         break;
+
+//  }
+
+
+//Question 18
+
+
+// var num = window.prompt("Enter num")
+//  switch(num%2==0)
+//  {case true:
+//     document.write("num is even")
+//     break;
+
+// case false:
+//             document.write("num is odd")
+//         break;
+//  }
+
+
+//Question 19
+//  var num = window.prompt("Enter num")
+
+//  switch(num>0){
+// case true:
+//     document.write("num is +ve")
+//          break;
+
+
+//          case false:
+//       switch(num==0){
+
+//         case true:
+//             document.write("num is zero")
+// break;
+// case false: 
+// document.write("num is neg")
+//       }
+//    }
+
+//Question 20
+
+
+
+// var num1 = Number(window.prompt("Enter num"))
+//   var num2 = Number(window.prompt("Enter num"))
+// var sign =window.prompt("enter sign")
+
+// switch(sign)
+// {
+//     case"+":
+//  document.write(num1+num2);
+
+//  break;
+
+//  case"-":
+//  document.write(num1-num2);
+
+
+// break;
+//  case"*":
+//  document.write(num1*num2);
+
+// break;
+//  case"%":
+//  document.write(num1%num2);
+
+// break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
